7fa737026f7b7cdcd65a7702ef023b6bd988dcb2
MartinK <82144965+MartinKruess@users.noreply.github.com>
2022-10-26 09:18:46 +0200

    updaTE (#2)
    Co-authored-by: Martin <martinkr90@googlemail.com>

--- src/components/reusables/newSpan.jsx
+++ src/components/reusables/newSpan.jsx
@@ -23,8 +23,6 @@ export const CreateArticle = ({ i }) => {
     (e) => setCount(e.target.value.length), (e) => handleChange(e);
   };
 
-  var today = new Date();
-  const dateOfDay = today.toLocaleDateString("de-DE");
   const [count, setCount] = useState(0);
   return (
     <form className="createForm" action="">
@@ -62,25 +60,3 @@ export const CreateArticle = ({ i }) => {
         }
     ]
 */
-
-{
-  /* <form className="createForm" action="">
-<label htmlFor="">Titel</label>
-<input type="text" />
-<label htmlFor="">{dateOfDay}</label>
-<label className="content" htmlFor="">
-  Content
-</label>
-<textarea
-  name=""
-  id=""
-  cols="30"
-  rows="10"
-  maxLength={3000}
-  onChange={(e) => setCount(e.target.value.length)}
-></textarea>
-<p className="counter">{count}/3000</p>
-<input type="file" />
-<button type="submit">Create</button>
-</form> */
-}
