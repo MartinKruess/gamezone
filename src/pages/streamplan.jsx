844b372ee7e422287b783cb51fd3f977c8be1130
MartinK <82144965+RaikunGZ@users.noreply.github.com>
2023-05-13 00:48:55 +0200

    streamplan rework

--- src/pages/streamplan.jsx
+++ src/pages/streamplan.jsx
@@ -17,7 +17,7 @@ export const Streamplan = () => {
               autoPlay={true}
               muted={true}
               loop={true}
-              src="/videos/rl_vid_op.mp4"
+              src="/videos/rl.mp4"
             ></video>
           </div>
           <div className="social">
@@ -37,7 +37,7 @@ export const Streamplan = () => {
                 autoPlay={true}
                 muted={true}
                 loop={true}
-                src="/videos/realm_vid.mp4"
+                src="/videos/realm.mp4"
               ></video>
           </div>
           <div className="social">
@@ -46,53 +46,14 @@ export const Streamplan = () => {
           </div>
         </div>
         <div className="planCard">
-          <div className="day">Samstag</div>
+          <div className="day">Samstag und Sonntag</div>
           <div className="video">
-            <div className="videoWe">
-              <div className="videoLeft">
-                <video
-                  autoPlay={true}
-                  muted={true}
-                  loop={true}
-                  src="/videos/rl_vid_op.mp4"
-                ></video>
-              </div>
-              <div>
-                <video
-                  autoPlay={true}
-                  muted={true}
-                  loop={true}
-                  src="/videos/realm_vid.mp4"
-                ></video>
-              </div>
-            </div>
-          </div>
-          <div className="social">
-            <i className="fa-brands fa-twitch"></i>
-            <i className="fa-brands fa-youtube"></i>
-          </div>
-        </div>
-        <div className="planCard">
-          <div className="day">Sonntag</div>
-          <div className="video">
-            <div className="videoWe">
-              <div className="videoLeft">
-                <video
-                  autoPlay={true}
-                  muted={true}
-                  loop={true}
-                  src="/videos/rl_vid.mp4"
-                ></video>
-              </div>
-              <div>
-                <video
-                  autoPlay={true}
-                  muted={true}
-                  loop={true}
-                  src="/videos/realm_vid.mp4"
-                ></video>
-              </div>
-            </div>
+            <video
+              autoPlay={true}
+              muted={true}
+              loop={true}
+              src="/videos/combined.mp4"
+            ></video>
           </div>
           <div className="social">
             <i className="fa-brands fa-twitch"></i>
